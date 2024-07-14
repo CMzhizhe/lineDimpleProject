d4c7df9aaa66314ca402496aa297eb958df74be6
gxx <745570899@qq.com>
2024-07-14 13:32:55 +0800

    1、新增步骤view,未完成

---
+++ app/src/main/java/com/example/linedimpleproject/ui/MainActivityV7.java
@@ -0,0 +1,17 @@
+package com.example.linedimpleproject.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.linedimpleproject.R;
+
+public class MainActivityV7 extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_v7);
+    }
+}
