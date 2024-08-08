1aa2a398a366e3aab5d956bd6895685332de9bba
gaoxiaoxiong <gaoxiaoxiong@yolonet.io>
2024-08-08 18:12:20 +0800

    1、优化边缘部分是从矩形开始的问题

--- linelibrary/src/main/java/com/gxx/linelibrary/RecentView.java
+++ linelibrary/src/main/java/com/gxx/linelibrary/RecentView.java
@@ -2,10 +2,16 @@ package com.gxx.linelibrary;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.graphics.Region;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,7 +25,7 @@ public class RecentView  extends View {
     private int mCorner = 0;
     private Paint mUnderPaint = null;
     private Paint mForegroundPaint = null;
-
+    private Path mPath = new Path();
     public RecentView(Context context) {
         this(context,null);
     }
@@ -44,11 +50,14 @@ public class RecentView  extends View {
         mUnderPaint.setStyle(Paint.Style.FILL);
 
 
-        mForegroundPaint = new Paint();
+        mForegroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mForegroundPaint.setAntiAlias(true);
         mForegroundPaint.setColor(foregroundColor);
         mForegroundPaint.setStyle(Paint.Style.FILL);
+
     }
+
+
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
@@ -56,13 +65,24 @@ public class RecentView  extends View {
 
         canvas.drawRoundRect(new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight()), mCorner, mCorner, mUnderPaint);
 
-        float right = getMeasuredWidth() * mInputProgress + mCorner * 2;
-        if (right > getMeasuredWidth()){
-            right = getMeasuredWidth();
+        float rightPercent = getMeasuredWidth() * mInputProgress;
+        if (rightPercent <= mCorner * 2 ){
+            canvas.save();
+            mPath.reset();
+            mPath.addRoundRect(new RectF(0, 0,mCorner * 2, getMeasuredHeight()), mCorner,mCorner, Path.Direction.CW);
+            canvas.clipPath(mPath);
+            canvas.drawRoundRect(new RectF(0, 0, rightPercent , getMeasuredHeight()),mCorner,mCorner,mForegroundPaint);
+            canvas.restore();
+        }else {
+            if (rightPercent > getMeasuredWidth()){
+                rightPercent = getMeasuredWidth();
+            }
+            canvas.drawRoundRect(new RectF(0, 0, rightPercent , getMeasuredHeight()),mCorner,mCorner,mForegroundPaint);
         }
 
-        canvas.drawRoundRect(new RectF(0, 0, right , getMeasuredHeight()),mCorner,mCorner,mForegroundPaint);
     }
+
+
 
 
     public void setProgress(float progress){
